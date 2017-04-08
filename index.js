f48dadbea43952feab2709fc63b48f6a17b7a730
Richard Mands <richard@eduku.org>
2017-04-08 20:06:57 +0800

    update

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 console.log('Hello');
-
 let open = false;
-
 const hamburger = document.getElementById('hamburger');
-var spans = document.getElementsByClassName("span");
+const spans = document.getElementsByClassName('span');
+const navBoxMobs = document.getElementsByClassName('navBoxMob')
+const navItemMobs = document.getElementsByClassName('navItemMob')
+const hrs = document.getElementsByClassName('hr')
 
 function openMenu() {
-  console.log('in openMenu');
-  if (open == false){
+
+  if (open === false) {
     open = true;
-  };
+  } else {
+    open = false;
+  }
+
   if (open) {
-    for (var i = 0; i < spans.length; i++) {
-      spans[i].style.backgroundColor = 'purple'
-    } else {
-        for (var i = 0; i < spans.length; i++) {
-          spans[i].style.backgroundColor = 'orange'
-        };
+    for (let i = 0; i < spans.length; i++) {
+      spans[i].style.backgroundColor = '#247BA0';
     }
+    for (let i = 0; i < navBoxMobs.length; i++) {
+      navBoxMobs[i].style.opacity = '1';
+    }
+    for (let i = 0; i < navItemMobs.length; i++) {
+      navItemMobs[i].style.opacity = '1';
+    }
+
+    document.getElementsByClassName('navBarMob')[0].style.height = '410px';
+    document.body.hr.style.opacity = '1';
+  } else {
+    for (let i = 0; i < spans.length; i++) {
+      spans[i].style.backgroundColor = 'orange';
+    }
+    for (let i = 0; i < navBoxMobs.length; i++) {
+      navBoxMobs[i].style.opacity = '0';
+    }
+    for (let i = 0; i < navItemMobs.length; i++) {
+      navItemMobs[i].style.opacity = '0';
+    }
+
+    document.getElementsByClassName('navBarMob')[0].style.height = '50px';
   }
 }
 
